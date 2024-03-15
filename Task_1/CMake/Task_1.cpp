05c2e11a8d97ce4f675b4a28601bbddd97260599
kulyovdr <danil.kulev.03@gmail.com>
2024-03-15 17:41:45 +0700

    Исправил ошибки

--- Task_1/CMake/Task_1.cpp
+++ Task_1/CMake/Task_1.cpp
@@ -1,15 +1,21 @@
 ﻿#include <iostream>
 #define _USE_MATH_DEFINES
 #include <cmath>
-#include <math.h>
 #include <iomanip>
 #include <memory>
 #define LEN 10000000
 
+const double PI = 3.14159265358979323846;
+
 #if FL == 1
 using my_type = float;
+#define sinus(x) sin(x)
+#elif FL == 2
+using my_type = float;
+#define sinus(x) sinf(x)
 #else
 using my_type = double;
+#define sinus(x) sin(x)
 #endif
 
 my_type sin_array(std::shared_ptr<my_type[]> arr)
@@ -17,11 +23,7 @@ my_type sin_array(std::shared_ptr<my_type[]> arr)
     my_type sum = 0;
     for (int i = 0; i < LEN; i++)
     {
-<<<<<<< Updated upstream
-        arr[i] = sinus((2 * M_PI) * ((my_type)i / LEN));
-=======
-        arr[i] = sin((2 * M_PI) * (i / LEN));
->>>>>>> Stashed changes
+        arr[i] = sinus((2 * PI) * ((my_type)i / LEN));
         sum += arr[i];
     }
     return sum;
@@ -29,13 +31,7 @@ my_type sin_array(std::shared_ptr<my_type[]> arr)
 
 int main(int argc, char* argv[])
 {
-<<<<<<< Updated upstream
     std::shared_ptr<my_type[]> arr(new my_type[LEN]);
     std::cout << std::fixed << std::setprecision(30) << sin_array(arr) << std::endl;
-=======
-    my_type* arr = new my_type[LEN];
-    cout << fixed << setprecision(30) << sin_array(arr) << endl;
-    delete[] arr;
->>>>>>> Stashed changes
     return 0;
 }
